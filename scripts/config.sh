e1d2e9d2ec7c8034a23b6d1f2bbc4e299ae36e7c
snake <admin@viasnake.com>
2023-09-18 08:47:49 +0900

    feat: config.shを追加

---
+++ scripts/config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+username="MCPlayNetwork"
+email="admin@mcplay.biz"
+
+git config --global user.name "$username"
+git config --global user.email "$email"
+
+echo "Success: Configured git"
