96533fae526df15c1afdb08f83cd0a89b96b410f
snake <admin@viasnake.com>
2023-09-19 05:10:26 +0900

    feat: commit.shを追加

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,1 +1,15 @@
-# TODO
+#!/bin/bash
+
+data_dir="$HOME/data"
+
+for repo_dir in $data_dir/*; do
+  if [ -d "$repo_dir/.git" ]; then
+    echo "Processing Git repository in: $repo_dir"
+    cd "$repo_dir" || exit 1
+
+    git add .
+    git commit -m "kitsune-core: `date +'%Y-%m-%d %H:%M:%S'`" -m "`git diff --name-only --staged`"
+
+    echo "Completed processing repository in: $repo_dir"
+  fi
+done
