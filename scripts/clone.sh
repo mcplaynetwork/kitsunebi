b1afc997d299407c17baa3ba72e0754898ed08b7
snake <admin@viasnake.com>
2023-09-19 05:35:06 +0900

    feat: 引数でcloneするrepoを指定可能にする

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-repo_list_file="repos.txt"
-clone_dir="$HOME/data"
+if [ $# -eq 0 ]; then
+  echo "Error: Please provide the repository name as the first argument."
+  exit 1
+fi
 
-mkdir -p "$clone_dir"
+repo_name="$1"
+data_dir="$HOME/data"
+repo_url="git@github.com:mcplay-data/$repo_name.git"
 
-while IFS= read -r repo_name; do
-  repo_url="git@github.com:mcplay-data/$repo_name.git"
+mkdir -p "$data_dir"
 
-  git clone "$repo_url" "$clone_dir/$repo_name"
+git clone "$repo_url" "$data_dir/$repo_name"
 
-  if [ $? -eq 0 ]; then
-    echo "Success: Cloned $repo_name"
-  else
-    echo "Error: Failed to clone $repo_name"
-  fi
-done < "$repo_list_file"
+if [ $? -eq 0 ]; then
+  echo "Success: Cloned $repo_name"
+else
+  echo "Error: Failed to clone $repo_name"
+fi
