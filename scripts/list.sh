bd4a4b6c2e1e0b95d955950c5372f1ab57087630
snake <admin@viasnake.com>
2023-10-01 17:41:41 +0900

    refactor: フォーマット

--- scripts/list.sh
+++ scripts/list.sh
@@ -3,17 +3,17 @@
 DIST_DIR="$HOME/kitsune-core/dist"
 
 for repo_dir in "$DIST_DIR"/*; do
-    if [ -d "$repo_dir" ]; then
-        repo_name=$(basename "$repo_dir")
+  if [ -d "$repo_dir" ]; then
+    repo_name=$(basename "$repo_dir")
 
-        echo "Creating file list for $repo_name..."
+    echo "Creating file list for $repo_name..."
 
-        files=$(find "$repo_dir" -maxdepth 1 -type f -name "*.jar" -exec basename {} \;)
+    files=$(find "$repo_dir" -maxdepth 1 -type f -name "*.jar" -exec basename {} \;)
 
-        if [ -n "$files" ]; then
-            echo "$files" > "$repo_dir/list.txt"
-        fi
+    if [ -n "$files" ]; then
+      echo "$files" >"$repo_dir/list.txt"
     fi
+  fi
 done
 
 echo "Created file lists for each subdirectory."
