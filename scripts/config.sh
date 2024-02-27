697947268ae7b518f148891b7392394402b54709
snake <admin@viasnake.com>
2024-02-28 02:44:41 +0900

    refactor: 不要スクリプトの削除

--- scripts/config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-username="MCPlayNetwork"
-email="admin@mcplay.biz"
-
-git config --global user.name "$username"
-git config --global user.email "$email"
-
-echo "Success: Configured git"
