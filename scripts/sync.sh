b73a266bf2b700412c755817df46d36a4e8bc7d8
snake <admin@viasnake.com>
2024-02-28 12:17:54 +0900

    refactor: sync.sh

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,33 +1,101 @@
 #!/bin/bash
 
-# if [ $# -lt 1 ]; then
-#   echo "Usage: $0 <password>"
-#   exit 1
-# fi
+SCRIPT_NAME=$(basename "$0")
+BASE_DIR="/opt/kitsunebi/data"
+SSH_PRIVATE_KEY="/opt/kitsunebi/secrets/kitsunebi"
+SSH_USER="infra"
+SSH_HOST="10.210.6.1"
 
-dist_server="infra@10.210.6.1"
-dest_base_dir="$HOME/kitsunebi/data"
-source_base_dir="~/dist/kitsunebi"
+# START: common functions
+Logger() {
+  local log_level="$1"
+  local message="$2"
+  local timestamp=$(date +'%Y-%m-%d %H:%M:%S')
+  echo "[$timestamp] [$log_level] - $message"
+}
 
-if [ ! -d "$dest_base_dir" ]; then
-  echo "Error: Destination base directory '$dest_base_dir' does not exist."
-  exit 1
-fi
-
-for dest_dir in "$dest_base_dir"/*; do
-  if [ ! -d "$dest_dir" ]; then
-    echo "Error: Destination directory '$dest_dir' does not exist."
-    exit 1
-  fi
-  dir_name=$(basename "$dest_dir")
-  dest_dir="$dest_base_dir/$dir_name"
-  source_dir="$source_base_dir/$dir_name"
-
-  sshpass -p "$ADMIN_PASSWORD" rsync -av --delete --exclude='*/' --include='*.jar' --include='list.txt' "$dist_server:$source_dir/" "$dest_dir/plugins/"
-
-  if [ $? -eq 0 ]; then
-    echo "Success: Jar files from '$dir_name' copied successfully."
+Load_env() {
+  if [ -f "$ENV_FILE" ]; then
+    source "$ENV_FILE"
   else
-    echo "Error: An error occurred while copying jar files from '$dir_name'."
+    Logger "ERROR" "$ENV_FILE not found"
+    return 1
   fi
-done
+}
+
+Check_env() {
+  if [ -z "$hoge" ] || [ -z "$fuga" ]; then
+    Logger "ERROR" "Required environment variables are not set"
+    return 1
+  fi
+}
+
+Check_directory_exists() {
+  local dir="$1"
+  if [ ! -d "$dir" ]; then
+    Logger "ERROR" "Directory $dir not found"
+    return 1
+  fi
+}
+
+Check_file_exists() {
+  local file="$1"
+  if [ ! -f "$file" ]; then
+    Logger "ERROR" "File $file not found"
+    return 1
+  fi
+}
+# END: common functions
+
+Main() {
+  local dir="$1"
+
+  Logger "INFO" "=== Starting $SCRIPT_NAME ==="
+
+  # ディレクトリが存在するか確認
+  Check_directory_exists "$dir" || {
+    exit 1
+  }
+
+  # SSH private keyが存在するか確認
+  Check_file_exists "$SSH_PRIVATE_KEY" || {
+    exit 1
+  }
+
+  # ディレクトリ内の全てのディレクトリに対して処理を実行
+  for dir in "$dir"/*; do
+    # ディレクトリの場合のみ処理を実行
+    if [ -d "$dir" ]; then
+      Perform "$dir"
+
+      # 終了コードを確認
+      if [ $? -ne 0 ]; then
+        Logger "ERROR" "Backup failed for directory: $dir"
+      fi
+
+    fi
+  done
+
+  Logger "INFO" "=== Finished $SCRIPT_NAME ==="
+}
+
+Perform() {
+  local dir="$1"
+  local dir_name=$(basename "$dir")
+
+  local source_dir="~/dist/kitsunebi/$dir_name"
+  local dest_dir="$dir/plugins"
+
+  Logger "INFO" "Syncing jar files..."
+
+  rsync -e "ssh -i $SSH_PRIVATE_KEY" -av --delete --exclude='*/' --include='*.jar' --include='list.txt' "$SSH_USER@$SSH_HOST:$source_dir/" "$dest_dir"
+
+  # 終了コードを確認
+  if [ $? -eq 0 ]; then
+    Logger "INFO" "Successfully synced jar files from $dir_name."
+  else
+    Logger "ERROR" "An error occurred while syncing jar files from $dir_name."
+  fi
+}
+
+Main "$BASE_DIR"
